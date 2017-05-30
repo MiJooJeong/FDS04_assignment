0ca2c01939b70524cdd3125df31ff6f834f2ccf2
MiJooJeong <uuuio1224@gmail.com>
2017-05-31 00:11:10 +0900

    Create student.js

---
+++ assignment_0530/student.js
@@ -0,0 +1,125 @@
+// -----------------------------------------
+// 학생 데이터 -> 10명
+// -----------------------------------------
+// 이름
+// 성별
+// 나이
+// 취미
+// 학교
+// 학년
+// 전공
+// 부전공
+// 가족 구성원
+
+var student_data = [];
+
+student_data['student_1'] = {
+  'name': 'mj',
+  'gender': 'female',
+  'age': '30',
+  'hobby': 'watching movie',
+  'school': 'mj school',
+  'grade': '1th',
+  'major': 'Electronic engineering',
+  'second_major': 'management',
+  'family': '3'
+};
+student_data['student_2'] = {
+  'name': 'minjeong',
+  'gender': 'female',
+  'age': '30',
+  'hobby': 'driving car',
+  'school': 'h school',
+  'grade': '2th',
+  'major': 'management',
+  'second_major': 'none',
+  'family': '4'
+};
+student_data['student_3'] = {
+  'name': 'ms',
+  'gender': 'male',
+  'age': '29',
+  'hobby': 'playing game',
+  'school': 'k school',
+  'grade': '3th',
+  'major': 'engineering',
+  'second_major': 'none',
+  'family': '3'
+};
+student_data['student_4'] = {
+  'name': 'ssong',
+  'gender': 'female',
+  'age': '31',
+  'hobby': 'listening music',
+  'school': 'u school',
+  'grade': '4th',
+  'major': 'art',
+  'second_major': 'music',
+  'family': '2'
+};
+student_data['student_5'] = {
+  'name': 'jane',
+  'gender': 'female',
+  'age': '31',
+  'hobby': 'reading a book',
+  'school': 'k school',
+  'grade': '5th',
+  'major': 'law',
+  'second_major': 'history',
+  'family': '5'
+};
+student_data['student_6'] = {
+  'name': 'suzy',
+  'gender': 'female',
+  'age': '27',
+  'hobby': 'none',
+  'school': 'y school',
+  'grade': '6th',
+  'major': 'education',
+  'second_major': 'none',
+  'family': '5'
+};
+student_data['student_7'] = {
+  'name': 'jin',
+  'gender': 'male',
+  'age': '23',
+  'hobby': 'watching a movie',
+  'school': 'fg school',
+  'grade': '7th',
+  'major': 'history',
+  'second_major': 'education',
+  'family': '2'
+};
+student_data['student_8'] = {
+  'name': 'gd',
+  'gender': 'male',
+  'age': '26',
+  'hobby': 'sing a song',
+  'school': 'yg school',
+  'grade': '8th',
+  'major': 'music',
+  'second_major': 'none',
+  'family': '6'
+};
+student_data['student_9'] = {
+  'name': 'yoo',
+  'gender': 'male',
+  'age': '32',
+  'hobby': 'dancing',
+  'school': 'w school',
+  'grade': '9th',
+  'major': 'art',
+  'second_major': 'none',
+  'family': '5'
+};
+student_data['student_10'] = {
+  'name': 'yoo',
+  'gender': 'male',
+  'age': '32',
+  'hobby': 'dancing',
+  'school': 'w school',
+  'grade': '10th',
+  'major': 'art',
+  'second_major': 'none',
+  'family': '5'
+};
